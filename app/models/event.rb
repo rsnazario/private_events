65fa59c9ccd375f365bcadd40b1817895740f078
SevlaMare <sevilamare@gmail.com>
2020-06-01 12:49:25 -0300

    event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  # belongs_to :creator, class_name: 'User' # creator of event
+  belongs_to :creator, class_name: 'User' # creator of event
   
   has_many :attendances, foreign_key: :attended_event_id, class_name: 'Attendance' # guests will come
   has_many :guests, through: :attendances, source: :attendee
