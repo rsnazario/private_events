93ae7705fa0344792844317e58600e96716525cb
SevlaMare <sevilamare@gmail.com>
2020-06-03 12:24:32 -0300

    config to clean db between transations

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,11 +40,15 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+
 RSpec.configure do |config|
   # Remove if not using ActiveRecord or fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  # don`t reset db for each new transaction
+  
+  # clean setup after transaction
   config.use_transactional_fixtures = true
+
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 end
