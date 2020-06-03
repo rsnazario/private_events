aa6905f26a0ece5166c73265b99c5bcb7238b364
SevlaMare <sevilamare@gmail.com>
2020-06-03 13:30:11 -0300

    linter fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,10 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    if userx = User.find_by(username: params[:username_in])
-      cookies[:current_user_id] = userx.id
-      redirect_to user_path(cookies[:current_user_id])
-    end
+    return unless (userx = User.find_by(username: params[:username_in]))
+
+    cookies[:current_user_id] = userx.id
+    redirect_to user_path(cookies[:current_user_id])
   end
 
   def destroy
