8de7df247e38d1de7e13c3f88971d1c52486b1bf
SevlaMare <sevilamare@gmail.com>
2020-06-03 12:26:56 -0300

    database cleanup gem setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,4 +51,29 @@ RSpec.configure do |config|
 
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+
+  # re-build db when start test suit
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  # capybara setup
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.include Capybara::DSL
 end
