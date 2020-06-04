14baf3c24167971586f8e883a4a1f2a4483fe5a1
rsnazario <rafael.s.nazario@gmail.com>
2020-06-04 15:17:22 -0300

    Rubocop Fix

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,9 +1,10 @@
 module EventsHelper
   def upcoming_events_list(events)
     content_tag :ul do
-      events.upcoming.map do |line| concat(
-        content_tag(:li, (link_to line.name, event_path(line.id)))
-      )
+      events.upcoming.map do |line|
+        concat(
+          content_tag(:li, (link_to line.name, event_path(line.id)))
+        )
       end
     end
   end
