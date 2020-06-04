616570c2dbd0dc38b77aef4ad30f1952561ed856
rsnazario <rafael.s.nazario@gmail.com>
2020-06-04 15:57:02 -0300

    Applied corrections on view user#show

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,31 @@
 module UsersHelper
+  def created_list(creator)
+    content_tag :ul do
+      creator.event_creator.map do |line|
+        concat(
+          content_tag(:li, (link_to line.name, event_path(line.id)))
+        )
+      end
+    end
+  end
+
+  def attended_list(gone)
+    content_tag :ul do
+      gone.map do |line|
+        concat(
+          content_tag(:li, (link_to line.name, event_path(line.id)))
+        )
+      end
+    end
+  end
+
+  def to_go_list(to_go)
+    content_tag :ul do
+      to_go.map do |line|
+        concat(
+          content_tag(:li, (link_to line.name, event_path(line.id)))
+        )
+      end
+    end
+  end
 end
