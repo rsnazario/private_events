5fa4b3daa7a63bebbb3395920e694723cabc6ad3
SevlaMare <sevilamare@gmail.com>
2020-06-04 16:46:45 -0300

    split buttons logic

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,10 +4,15 @@ module SessionsHelper
     current_user
   end
 
-  def user_btn_actions
+  def user_btn_create_event
     return unless current_user
 
     content_tag(:a, (link_to 'Create Event', new_event_path))
+  end
+
+  def user_btn_profile
+    return unless current_user
+
     content_tag(:a, (link_to 'Profile', user_path(current_user)))
   end
 
@@ -17,10 +22,15 @@ module SessionsHelper
     content_tag(:a, (link_to 'LOGOUT', new_session_path, method: :delete))
   end
 
-  def btn_signup
+  def btn_register
     return if current_user
 
     content_tag(:a, (link_to 'REGISTER', new_user_path))
+  end
+
+  def btn_login
+    return if current_user
+
     content_tag(:a, (link_to 'LOGIN', new_session_path))
   end
 end
