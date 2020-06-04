780103c4bda551583c1f6ea96e498d2b6bd5ea31
SevlaMare <sevilamare@gmail.com>
2020-06-04 15:13:01 -0300

    turn loop into function, list events upcoming

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,11 @@ module SessionsHelper
     current_user = User.find_by(id: cookies[:current_user_id])
     current_user
   end
+
+  def user_buttons
+    return unless current_user
+
+    content_tag(:a, (link_to 'Create Event', new_event_path))
+    content_tag(:a, (link_to 'Profile', user_path(current_user)))
+  end
 end
