637a3991b147999054912c827407dd5b5a53c26c
SevlaMare <sevilamare@gmail.com>
2020-06-04 14:27:58 -0300

    abstract if else logic from view to helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def user_buttons
+    return unless current_user
+
+    content_tag(:a, (link_to 'Create Event', new_event_path))
+    content_tag(:a, (link_to 'Profile', user_path(current_user)))
+  end
 end
