b0e12f00c9a0ab0188add7372bc9ec55d851c6e7
rsnazario <rafael.s.nazario@gmail.com>
2020-06-04 15:28:11 -0300

    Applied corrections on view event#show

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -12,7 +12,19 @@ module EventsHelper
   def previous_events_list(events)
     content_tag :ul do
       events.previous.map do |line|
-        concat(content_tag(:li, (link_to line.name, event_path(line.id))))
+        concat(
+          content_tag(:li, (link_to line.name, event_path(line.id)))
+        )
+      end
+    end
+  end
+
+  def guest_list(event)
+    content_tag :ul do
+      event.guests.map do |line|
+        concat(
+          content_tag(:li, line.username)
+        )
       end
     end
   end
