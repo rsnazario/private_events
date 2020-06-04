31ea1ff393a9c4023b821adc4d8165b0197c4930
rsnazario <rafael.s.nazario@gmail.com>
2020-06-04 15:22:23 -0300

    Applied corrections on view event#index

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -8,4 +8,12 @@ module EventsHelper
       end
     end
   end
+
+  def previous_events_list(events)
+    content_tag :ul do
+      events.previous.map do |line|
+        concat(content_tag(:li, (link_to line.name, event_path(line.id))))
+      end
+    end
+  end
 end
