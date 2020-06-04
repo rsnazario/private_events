473cb3a232b1dd1234068a970a4f8f13f4fe9e49
SevlaMare <sevilamare@gmail.com>
2020-06-04 16:41:12 -0300

    remove all if else from views]

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,10 +4,23 @@ module SessionsHelper
     current_user
   end
 
-  def user_buttons
+  def user_btn_actions
     return unless current_user
 
     content_tag(:a, (link_to 'Create Event', new_event_path))
     content_tag(:a, (link_to 'Profile', user_path(current_user)))
+  end
+
+  def btn_logout
+    return unless current_user
+
+    content_tag(:a, (link_to 'LOGOUT', new_session_path, method: :delete))
+  end
+
+  def btn_signup
+    return if current_user
+
+    content_tag(:a, (link_to 'REGISTER', new_user_path))
+    content_tag(:a, (link_to 'LOGIN', new_session_path))
   end
 end
